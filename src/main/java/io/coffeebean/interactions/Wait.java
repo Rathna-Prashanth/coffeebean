aca05508d38e7f02a3ddd7183430745b8a7a8917
Rathna Prashanth <rathnaprashanth.m@gmail.com>
2021-11-22 21:37:30 +0530

    Java doc updated

--- src/main/java/io/coffeebean/interactions/Wait.java
+++ src/main/java/io/coffeebean/interactions/Wait.java
@@ -2,7 +2,15 @@ package io.coffeebean.interactions;
 
 public interface Wait {
 
+    /**
+     * @param wait Wait unit in seconds
+     * @param pollingTime Polling Time in seconds
+     * @return Self reference
+     */
     Interactive setCustomWait(int wait,int pollingTime);
 
+    /**
+     * @return Self reference
+     */
     Interactive setGlobalWait();
 }
